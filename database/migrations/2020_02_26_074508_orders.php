abae186502cf12f79548128652a02413e9292a89
ep67 <aepxy025@gmail.com>
2020-02-26 15:08:51 +0700

    init commit

---
+++ database/migrations/2020_02_26_074508_orders.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Orders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('invoice')->unique();
+            $table->bigInter('id_user');
+            $table->bigInteger('id_customer');
+            $table->bigInteger('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
